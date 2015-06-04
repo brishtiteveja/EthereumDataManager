7796130b029f2898feffa0f6e9336ba746bd3c65
Zehady Abdullah Khan <brishtiteveja@gmail.com>
2015-06-04 11:39:42 -0400

    pycurl POST request correction.

--- EthereumDataManager.py
+++ EthereumDataManager.py
@@ -1,14 +1,15 @@
-import pycurl
+import pycurl,json 
 from StringIO import StringIO
 
-buffer = StringIO('{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":83}')
-c = pycurl.Curl()
-c.setopt(c.URL, 'localhost:8545')
-c.setopt(c.WRITEDATA, buffer)
-c.perform()
-c.close()
+data = json.dumps('{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":83}')
 
-body = buffer.getvalue()
-# Body is a string in some encoding.
-# In Python 2, we can print it without knowing what the encoding is.
-print(body)
+ethereum_url = 'localhost:8545'
+
+c = pycurl.Curl()
+c.setopt(c.URL, ethereum_url)
+c.setopt(pycurl.HTTPHEADER, ['X-Postmark-Server-Token: API_TOKEN_HERE','Accept: application/json'])
+c.setopt(pycurl.POST, 1)
+c.setopt(pycurl.POSTFIELDS, data)
+response = c.perform()
+print response
+c.close()
