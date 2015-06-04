b8b1e03a735fa0af4ca06515326ccb0ec7e414e4
Zehady Abdullah Khan <brishtiteveja@gmail.com>
2015-06-04 12:39:20 -0400

    Block number and block info retrieval using cURL POST request from ethereum test net is successfully implemented.

--- EthereumDataManager.py
+++ EthereumDataManager.py
@@ -1,15 +1,36 @@
-import pycurl,json 
+import pycurl, json
+from pprint import pprint
 from StringIO import StringIO
-
-data = json.dumps('{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":83}')
 
 ethereum_url = 'localhost:8545'
 
-c = pycurl.Curl()
-c.setopt(c.URL, ethereum_url)
-c.setopt(pycurl.HTTPHEADER, ['X-Postmark-Server-Token: API_TOKEN_HERE','Accept: application/json'])
-c.setopt(pycurl.POST, 1)
-c.setopt(pycurl.POSTFIELDS, data)
-response = c.perform()
-print response
-c.close()
+
+def curl_req(url, data):
+    storage = StringIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(pycurl.POSTFIELDS, data)
+    c.setopt(c.WRITEFUNCTION, storage.write)
+    c.setopt(c.VERBOSE, False)
+    c.perform()
+    c.close()
+    content = storage.getvalue()
+    return content
+
+
+# Getting Block Number
+data_blockNumber = '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":83}'
+
+resp = curl_req(ethereum_url, data_blockNumber)
+body = json.loads(resp)
+
+blockNumber = int(body['result'], 16)
+print blockNumber
+
+# Getting Block by Number
+blockNum = hex(45367)
+data_blockByNumber = '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["0x1b4", true],"id":1}'
+resp = curl_req(ethereum_url, data_blockByNumber)
+body = json.loads(resp)
+print body
+
